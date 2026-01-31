ae9a86457568331cd822d7624eee66b3b433cde0
Carlos Alexandre <karurosualexandresouza123@gmail.com>
2026-01-30 22:09:22 -0400

    Adicionei favicon

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import logging
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from app.routes import router
 
@@ -13,6 +14,7 @@ logging.basicConfig(
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
 app.include_router(router)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
